89c56619a316ab0dfae67924740541540fb2b9c4
Pierri Alexander Vidmar <pierrialexandervidmar@gmail.com>
2023-08-26 11:22:15 -0300

    Commit JDBC
    Commit JDBC

---
+++ Course/jdbc1/src/db/DB.java
@@ -0,0 +1,62 @@
+package db;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DB {
+	
+	private static Connection conn = null;
+	
+	/**
+	 * Método para gerar uma conexão com o banco de dados
+	 * @return
+	 */
+	public static Connection getConnection() {
+		if(conn == null) {
+			try {
+				Properties props = loadProperties();
+				String url = props.getProperty("dburl");
+				conn = DriverManager.getConnection(url, props);
+			}
+			catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+		return conn;
+	}
+	
+	/**
+	 * Método que irá carregar os dados do arquivo db.properties
+	 * @return
+	 */
+	private static Properties loadProperties() {
+		try (FileInputStream fs = new FileInputStream("db.properties")){
+			Properties props = new Properties();
+			props.load(fs);
+			return props;
+		} catch (IOException e) {
+			throw new DbException(e.getMessage());
+		}
+	}
+	
+	/**
+	 * Método para fechar uma conexão com o banco de dados
+	 */
+	public static void closeConnection() {
+		if(conn != null) {
+			try {
+				conn.close();
+			}
+			catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+			
+		}
+	}
+	
+	
+}
